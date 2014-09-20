424cebabfa1bebeba0b0757135cd963612f16733
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-09-20 11:52:29 +0900

    setup rspec and CI service

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ML service for hokui.net
 =========================
+
+[![Build Status](https://travis-ci.org/hokui/mailing_list.svg?branch=master)](https://travis-ci.org/hokui/mailing_list)
+[![Code Climate](https://codeclimate.com/github/hokui/mailing_list/badges/gpa.svg)](https://codeclimate.com/github/hokui/mailing_list)
+[![Test Coverage](https://codeclimate.com/github/hokui/mailing_list/badges/coverage.svg)](https://codeclimate.com/github/hokui/mailing_list)
---
+++ spec/rails_helper.rb
@@ -0,0 +1,31 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
+ENV["RAILS_ENV"] ||= 'test'
+require 'spec_helper'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'factory_girl'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.include Helpers
+
+  config.infer_spec_type_from_file_location!
+
+  config.use_transactional_fixtures = true
+  config.include FactoryGirl::Syntax::Methods
+  config.before :all do
+    FactoryGirl.reload
+  end
+  config.before :suite do
+    DatabaseRewinder.clean_all
+    FactoryGirl.lint
+  end
+  config.before :each do
+    DatabaseRewinder.clean
+  end
+end
