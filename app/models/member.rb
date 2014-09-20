115d10a859d2ac4d8bb5f1c0bb928cb476ecb4ea
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-09-20 17:08:57 +0900

    spec for Member validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,15 @@
 class Member < ActiveRecord::Base
   has_and_belongs_to_many :lists
 
-  validates(:name)      { presence }
-  validates(:email)     { presence; uniqueness(scope: :email_sub) }
-  validates(:email_sub) {           uniqueness(scope: :email, allow_blank: true) }
+  validates(:name)  { presence }
+  validates(:email) { presence }
+  validate :uniqueness_between_email_and_email_sub
+
+  private
+
+  def uniqueness_between_email_and_email_sub
+    emails = Member.pluck(:email, :email_sub).flatten.select { |e| !e.blank? }
+    errors.add(:email, "has already been taken") if emails.include?(email)
+    errors.add(:email_sub, "has already been taken") if emails.include?(email_sub)
+  end
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Member, :type => :model do
+  describe "validation" do
+    it "ensures uniqueness between email/email_sub, while allowing email_sub to be blank" do
+      Member.create(name: "1", email: "1@example.com")                             # OK
+      Member.create(name: "2", email: "2@example.com")                             # OK
+      Member.create(name: "3", email: "3@example.com", email_sub: "3@example.net") # OK
+
+      Member.create(name: "4", email: "4@example.com", email_sub: "3@example.com") # Bad
+      Member.create(name: "5", email: "3@example.net", email_sub: "5@example.net") # Bad
+      Member.create(name: "6", email: "3@example.com", email_sub: "6@example.net") # Bad
+      Member.create(name: "7", email: "7@example.com", email_sub: "3@example.net") # Bad
+
+      expect(Member.pluck(:name)).to eq(["1", "2", "3"])
+    end
+  end
 end
