11b866c6eb9592147d3a09966926ecfbf2e6f513
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-10-03 00:17:04 +0900

    add http_basic module

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,9 +1,12 @@
 class Api::ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+
   before_action :authenticate
 
   private
 
   def authenticate
+    authenticate_or_request_with_http_basic
     authenticate_or_request_with_http_basic do |user, pass|
       user == ENV["HTTP_USER"] && pass = ENV["HTTP_PASS"]
     end
